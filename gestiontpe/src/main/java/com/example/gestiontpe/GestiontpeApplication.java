f3c9d2a18214b2b1af604212df04c573b76c2304
jppalominor <j.palominor@hotmail.com>
2023-10-13 15:50:14 -0500

    Creacion proyecto, configuracion inicial properties, dbconexion

---
+++ gestiontpe/src/main/java/com/example/gestiontpe/GestiontpeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestiontpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestiontpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestiontpeApplication.class, args);
+	}
+
+}
