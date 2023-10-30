b320a128934a2dbe4da54e2a9b1b25b22cdb0b63
jppalominor <j.palominor@hotmail.com>
2023-10-29 19:45:00 -0500

    implementacion stack HorarioCargadores

---
+++ gestiontpe/src/main/java/repository/HorarioCargadoresRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import models.HorarioCargadores;
+
+public interface HorarioCargadoresRepository extends JpaRepository<HorarioCargadores, Integer> {
+
+    
+}
